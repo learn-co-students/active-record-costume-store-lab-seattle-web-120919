f487ffb62525f76fc4956150a6e7457283e8d20d
github username <github email address>
2019-12-31 02:37:11 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees #num of employees
+            t.boolean :still_in_business #whether or not it's still in business
+            t.datetime :opening_time
+            t.datetime :closing_time
+        end
+    end
+end
